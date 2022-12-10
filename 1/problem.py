a0bf95f798a7b76d23350be048b963a8ea9ba9bb
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2022-12-09 23:38:25 -0600

    add day 1 problem 2 solution

--- 1/problem.py
+++ 1/problem.py
@@ -15,7 +15,21 @@ def problem1(input):
             running_total = running_total + int(input[n])
     return max
 
+def problem2(input):
+    full_calorie_list = []
+    running_total = 0
+    for n in range(len(input)):
+        line = input[n]
+        if line == '\n':
+            full_calorie_list.append(running_total)
+            running_total = 0
+        else:
+            running_total = running_total + int(input[n])
+    
+    full_calorie_list = sorted(full_calorie_list, reverse=True)
+    return sum(full_calorie_list[0:3])
 
 # program starts here
 input = read_input('input.txt')
 print(problem1(input))
+print(problem2(input))
